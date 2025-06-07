37fa393f35ed9def071453fdf3154ca222f130a2
Alessandro Loddo <59955660+Mamoru3@users.noreply.github.com>
2025-06-07 22:31:03 +0100

    Added plot visualisation, working on feature correlation and analysis

---
+++ china_cancer_patients_map_visualizer_data.py
@@ -0,0 +1,21 @@
+import folium
+
+# Center on China
+province_coords = {
+    "Guangdong": (23.1291, 113.2644),    # Guangzhou
+    "Shandong": (36.6512, 117.1201),     # Jinan
+    "Henan": (34.7466, 113.6254),        # Zhengzhou
+    "Sichuan": (30.5728, 104.0668),      # Chengdu
+    "Jiangsu": (32.0603, 118.7969),      # Nanjing
+    "Anhui": (31.8612, 117.2857),        # Hefei
+    "Hunan": (28.1941, 112.9823),        # Changsha
+    "Hebei": (38.0428, 114.5149),        # Shijiazhuang
+    "Zhejiang": (30.2741, 120.1551),     # Hangzhou
+    "Beijing": (39.9042, 116.4074),      # Beijing (direct-admin municipality)
+    "Shanghai": (31.2304, 121.4737),     # Shanghai (direct-admin municipality)
+    "Xinjiang": (43.8256, 87.6168),      # Urumqi
+    "Qinghai": (36.6171, 101.7782),      # Xining
+}
+
+
+
